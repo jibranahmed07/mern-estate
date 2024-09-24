8e88b09a1d30aa8513cc82d26d993df6fd262a18
jibranahmed07 <jibranahmed052@gmail.com>
2024-09-24 15:47:37 -0400

    Intial Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/SignIn';
+import Signout from './pages/Signout';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+
+
+
+
+export default function App() {
+  return (
+    <h1 className='text-red-700'>App</h1>
+
+  )
+}
